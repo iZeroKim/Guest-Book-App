acddad94dc4f25138efa202ac007ccfd8c5ff930
iZeroKim <kimgichuru6@gmail.com>
2022-11-12 20:35:27 +0300

    Add floating action to display dialog that captures a guest's details

--- lib/views/home/home.dart
+++ lib/views/home/home.dart
@@ -6,6 +6,7 @@ class Home extends StatefulWidget {
 }
 
 class _State extends State<Home> {
+  final TextEditingController eCtrl =  TextEditingController();
   final names = ["Samuel Kimani", "Allan Doe", "John Doe"];
   final times = [
     "11/11/2022, 8:30 am",
@@ -26,6 +27,7 @@ class _State extends State<Home> {
         ),
         body: Column(
             children: <Widget>[
+
               Expanded(
                   child: ListView.builder(
                       itemCount: names.length,
@@ -39,9 +41,55 @@ class _State extends State<Home> {
                                 backgroundImage: AssetImage("assets/react.png"),child: Text(name.toUpperCase()),backgroundColor: Colors.deepPurple,),
                                 trailing: Icon(icons[0])));
                       })
-              )
+              ),
+
             ]
-        )
+        ),
+      floatingActionButton: FloatingActionButton.extended(
+        label: Text("Add guest"),
+        icon: Icon(Icons.add),
+        backgroundColor: Colors.deepPurple,
+        onPressed: () {
+
+          setState(() {});
+          showDialog(
+              context: context,
+              builder: (context) {
+                return AlertDialog(
+                  title: Text('Guest name'),
+                  content: TextField(
+                    onChanged: (value) {
+                      setState(() {
+                        names.add("Joe Biden");
+                        times.add("Now");
+                        eCtrl.clear();
+                      });
+                    },
+                    controller: eCtrl,
+                    decoration: InputDecoration(hintText: "Guest name"),
+                  ),
+                  actions: <Widget>[
+                    TextButton(
+                      child: Text('CANCEL'),
+                      onPressed: () {
+                        setState(() {
+                          Navigator.pop(context);
+                        });
+                      },
+                    ),
+                    TextButton(
+                      child: Text('OK'),
+                      onPressed: () {
+                        setState(() {
+                          Navigator.pop(context);
+                        });
+                      },
+                    ),
+                  ],
+                );
+              });
+        },
+      ),
     );
   }
 }
