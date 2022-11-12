9be4d1a798f264c162b8e103b4e37e6edbee4cff
iZeroKim <kimgichuru6@gmail.com>
2022-11-12 18:37:40 +0300

    Display guest name and time of entry list. Show first name two letters as an image for each guest

--- lib/views/home/home.dart
+++ lib/views/home/home.dart
@@ -6,8 +6,13 @@ class Home extends StatefulWidget {
 }
 
 class _State extends State<Home> {
-  final List<String> names = <String>['Aby', 'Aish', 'Ayan', 'Ben', 'Bob', 'Charlie', 'Cook', 'Carline'];
-  final List<int> msgCount = <int>[2, 0, 10, 6, 52, 4, 0, 2];
+  final names = ["Samuel Kimani", "Allan Doe", "John Doe"];
+  final times = [
+    "11/11/2022, 8:30 am",
+    "11/11/2022, 8:33 am",
+    "11/11/2022, 8:34 am"
+  ];
+  final icons = [Icons.delete];
 
   @override
   Widget build(BuildContext context) {
@@ -23,26 +28,17 @@ class _State extends State<Home> {
             children: <Widget>[
               Expanded(
                   child: ListView.builder(
-                      padding: const EdgeInsets.all(8),
                       itemCount: names.length,
-                      itemBuilder: (BuildContext context, int index) {
-                        return Container(
-                          height: 50,
-                          margin: EdgeInsets.all(2),
-                          color: msgCount[index]>=10? Colors.blue[400]:
-                          msgCount[index]>3? Colors.blue[100]: Colors.grey,
-                          child: Center(
-                              // Text('${names[index]} (${msgCount[index]})',
-                              //   style: TextStyle(fontSize: 18),
-                              // ),
-                              child: CircleAvatar(
-                                backgroundImage: AssetImage("assets/react.png"),child: Text('BA'),
-                              )
-                          ),
-
-                        );
-                      }
-                  )
+                      itemBuilder: (context, index) {
+                        var name = names[index][0] + names[index][1];
+                        return Card(
+                            child: ListTile(
+                                title: Text(names[index]),
+                                subtitle: Text(times[index]),
+                                leading: CircleAvatar(
+                                backgroundImage: AssetImage("assets/react.png"),child: Text(name.toUpperCase()),backgroundColor: Colors.deepPurple,),
+                                trailing: Icon(icons[0])));
+                      })
               )
             ]
         )
